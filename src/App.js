df97e8cb23d9a558d5c0b8483cc041640ea82de6
pollychen5911 <152253711+pollychen5911@users.noreply.github.com>
2023-12-19 16:39:23 +0800

    1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from 'react';
+import Axios from 'axios';
+
+function App() {
+    const [catFact, setCatFact] = useState("")
+    
+    useEffect(()=>{
+      Axios.get("https://catfact.ninja/fact").then((res)=>{
+        console.log(res.data);
+        setCatFact(res.data.fact);
+      }); 
+    }, []);
+  return (<div className="App">
+    {catFact}
+    </div> );
+}
+
+export default App;
